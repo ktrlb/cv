6f99b6673d69319be25ce1e98565fc2a7b211562
ktrlb <karliebold@gmail.com>
2022-08-13 08:59:56 -0500

    Added Gatsby

---
+++ .cache/page-ssr/routes/component---src-pages-index-jshead.js
@@ -0,0 +1,16 @@
+exports.id = "component---src-pages-index-jshead";
+exports.ids = ["component---src-pages-index-jshead"];
+exports.modules = {
+
+/***/ "./src/pages/index.js?export=head":
+/*!****************************************!*\
+  !*** ./src/pages/index.js?export=head ***!
+  \****************************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
